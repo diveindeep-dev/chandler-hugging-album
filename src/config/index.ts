df392a21ec1acaf84da6452bae6a38f117427bc1
hakyung <diveindeep.dev@gmail.com>
2023-01-06 20:53:51 +0900

    Fix SERVER_URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
-const SERVER_URL: string =
+const SERVER_URL: string | undefined =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3001'
-    : 'PRODUCT_URL';
+    : process.env.REACT_APP_PRODUCT_URL;
 
 export { SERVER_URL };
